811ac34f0a95ec040ac673230fee7274a30563e2
fiq <me@taufiqhdyt.com>
2023-08-13 18:43:53 +0700

    fix: middleware handle error

--- src/helpers/middleware.js
+++ src/helpers/middleware.js
@@ -2,23 +2,30 @@ import config from '#config' assert { type: 'json' };
 
 import m$auth from '#module/auth.module.js';
 
-import { response, jwt, logError } from '#helper/utils.js';
+import { jwt, logError, response } from '#helper/utils.js';
 
 export const checkSession = async (req, res, next) => {
-  const { headers } = req;
-  const token = headers?.authorization?.startsWith('Bearer')
-    ? headers?.authorization?.split(' ')?.[1]
-    : null;
-
-  if (!token) {
-    response.send(res, 401, 'Not Authenticated!');
-  }
-
   try {
+    const { headers } = req;
+
+    const token = headers?.authorization?.startsWith('Bearer')
+      ? headers?.authorization?.split(' ')?.[1]
+      : null;
+
+    if (!token) {
+      throw {
+        code: 401,
+        message: 'Not Authenticated!',
+      };
+    }
+
     const decoded = await jwt.verify(token, config.jwt.secret);
 
     if (!decoded) {
-      response.send(res, 401, 'Not Authenticated!');
+      throw {
+        code: 401,
+        message: 'Not Authenticated!',
+      };
     }
 
     const [{ password, ...user }] = await m$auth.login({ username: decoded.u });
